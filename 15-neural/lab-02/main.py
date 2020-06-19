445604e7b9ab7cf1922198b087358d02458810e2
Valentin <valkrepkiy@gmail.com>
2020-06-19 17:17:42 +0300

    update lab-02

--- 15-neural/lab-02/main.py
+++ 15-neural/lab-02/main.py
@@ -1,37 +1,37 @@
 import numpy
 from tensorflow import keras, config
 
+# Fix memory issue,
+# Restrict TensorFlow to only allocate 1GB * 5 of memory on the first GPU
 gpus = config.experimental.list_physical_devices('GPU')
 if gpus:
-    # Restrict TensorFlow to only allocate 1GB * 2 of memory on the first GPU
     try:
         config.experimental.set_virtual_device_configuration(
             gpus[0],
-            [config.experimental.VirtualDeviceConfiguration(memory_limit=4024 * 2)])
+            [config.experimental.VirtualDeviceConfiguration(memory_limit=1024 * 5)])
         logical_gpus = config.experimental.list_logical_devices('GPU')
         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
     except RuntimeError as e:
         # Virtual devices must be set before GPUs have been initialized
         print(e)
 
-# Задаем seed для повторяемости результатов
+# Fix random seed
 numpy.random.seed(42)
 
-# Загружаем данные
-(X_train, y_train), (X_test, y_test) = keras.datasets.cifar10.load_data()
+# Fetch CIFAR-10 data
+(train_images, train_labels), (test_images,
+                               test_labels) = keras.datasets.cifar10.load_data()
 
-# Нормализуем данные
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
+# Normalize data
+train_images = train_images / 255.0
+test_images = test_images / 255.0
 
-# Преобразуем метки в категории
-Y_train = keras.utils.to_categorical(y_train, 10)
-Y_test = keras.utils.to_categorical(y_test, 10)
+# Convert enum to category
+train_labels = keras.utils.to_categorical(train_labels, 10)
+test_labels = keras.utils.to_categorical(test_labels, 10)
 
 model = keras.models.Sequential()
-# "3" stands for RGB 3 channels, 32x32 - image diminsions
+# input_shape - 32x32 - image diminsions, 3 channels (RGB)
 model.add(keras.layers.Conv2D(
     32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(keras.layers.Conv2D(32, (3, 3), activation='relu'))
@@ -47,16 +47,18 @@ model.add(keras.layers.Dropout(0.5))
 # "10" is our classes count
 model.add(keras.layers.Dense(10, activation='softmax'))
 
+print(model.summary())
+
 sgd = keras.optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
 
-model.fit(X_train, Y_train,
+model.fit(train_images, train_labels,
           batch_size=32,
           epochs=25,
           validation_split=0.1,
           shuffle=True)
 
-scores = model.evaluate(X_test, Y_test, verbose=0)
+scores = model.evaluate(test_images, test_labels, verbose=0)
 print("Точность работы на тестовых данных: %.2f%%" % (scores[1]*100))
