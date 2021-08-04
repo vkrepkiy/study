a2b878a72ff97f3c9dc3dc4afd6f63333dd67bae
Valentin Krepkiy <valkrepkiy@gmail.com>
2021-08-04 15:56:57 +0300

    add binary gap

---
+++ src/interview-preparations/tasks/binary-gap.spec.ts
@@ -0,0 +1,27 @@
+describe("binary-gap", () => {
+  it.each([[32, 0]])("should work", (n, expected) => {
+    expect(solution(n)).toBe(expected);
+  });
+});
+
+function solution(n: number) {
+  const str = n.toString(2);
+  let gapStart = -1;
+  let maxGap = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    // catch the gap start
+    if (str[i] === "0" && str[i - 1] === "1") {
+      gapStart = i;
+      continue;
+    }
+
+    // catch the gap end
+    if (str[i] === "1" && gapStart > -1) {
+      maxGap = Math.max(maxGap, i - gapStart);
+      gapStart = -1;
+    }
+  }
+
+  return maxGap;
+}
---
+++ src/interview-preparations/tasks/mushrooms.spec.ts
@@ -0,0 +1,25 @@
+describe(`You are given a non-empty, zero-indexed array A of n (1 <= n <= 100 000) integers
+a0, a1, . . . , an−1 (0 <= ai <= 1 000). This array represents number of mushrooms growing on the
+consecutive spots along a road.
+
+You are also given integers k and m (0 <= k, m < n).
+
+A mushroom picker is at spot number k on the road and should perform m moves. In
+one move she moves to an adjacent spot. She collects all the mushrooms growing on spots
+she visits. The goal is to calculate the maximum number of mushrooms that the mushroom
+picker can collect in m moves`, () => {
+  it("should work", () => {});
+});
+
+function getMax(arr: number[], position: number, stepLimit: number): number {
+  const sumPrefixLeft = arr
+    .slice(0, position)
+    .reverse()
+    .map((value, i, arr) => {
+      return (arr[i - 1] || 0) + value;
+    });
+
+  const sumPrefixRight = arr.slice(position).map((value, i, arr) => {
+    return (arr[i - 1] || 0) + value;
+  });
+}
